d7b23803a60f8788f3a122c6e5e6692635600e60
XLong <zxlong0530@foxmail.com>
2021-05-11 16:38:06 +0800

    05111636

---
+++ helloTest/zzTestGit/ViewController.m
@@ -0,0 +1,37 @@
+//
+//  ViewController.m
+//  zzTestGit
+//
+//  Created by 赵兴隆 on 2018/5/21.
+//  Copyright © 2018年 赵兴隆. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "TestViewController.h"
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    //引用
+    [TestViewController add];
+    
+    NSLog(@"git的初次使用");
+    
+    [TestViewController plus];
+    
+    NSLog(@"git plus");
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
